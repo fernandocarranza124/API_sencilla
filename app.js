6aaa934ecb5556b1a60e27ff4cf6e5e5fd426a5d
Isacc Carranza <fernandocarranza124@gmail.com>
2026-04-27 22:07:47 -0600

    jest

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 app.get('/', (req,res) =>{
     res.send('Esta es una prueba sencilla')
 });
+app.get('/suma', (req,res) =>{
+    const { a, b } = req.query;
+    const resultado = Number(a) + Number(b);
+    res.json({ resultado });
+});
 
 
 module.exports = app;
